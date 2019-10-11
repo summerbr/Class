3541a21851d2bc5104a65311e619b18a566b809c
Summer Brown <summer-brown@comcast.net>
2019-10-11 08:57:59 -0500

    working

--- Week7/Tuesday/routes/account.js
+++ Week7/Tuesday/routes/account.js
@@ -13,15 +13,32 @@ router.get("/login", function(req, res) {
 })
 
 router.post("/login", function(req, res) {
-  console.log(req.session)
+  // console.log(req.session)
   if (req.session) {
     req.session.email = req.body.email
   }
   res.redirect("/account/dashboard");
 })
 
+router.post("/add", function(req, res) {
+  if (req.session) {
+      if (!req.session.tryFoods) req.session.tryFoods = [];
+      console.log(req.session)
+      console.log(req.body.food);
+      req.session.tryFoods.push({ food: req.body.food })
+  }
+  res.redirect("/account/dashboard")
+})
 router.get("/dashboard", (req, res) => {
-  res.render("account/dashboard");
+  console.log(req.session.tryFoods);
+  var data = {};
+  if (req.session && req.session.tryFoods) {
+    data.tryFoods = req.session.tryFoods;
+  } else {
+    data.message = "Add some food!"
+    console.log(req.session)
+  }
+  res.render("account/dashboard", data);
 })
 
 router.get("/logout", (req,res) => {
