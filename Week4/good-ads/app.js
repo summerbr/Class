9fcdac4acc1953fcdd5da8208efba755d5e4ba21
Summer Brown <summer-brown@comcast.net>
2019-11-26 18:59:22 -0600

    working toDo

---
+++ Week4/good-ads/app.js
@@ -0,0 +1,10 @@
+
+// call the getRandomAd function
+let ad = getRandomAd();
+// get the adImage by id
+document.getElementById('adImage').src = ad
+
+// set the src property to the new random ad image
+
+// let randomImage = document.getElementById('adImage');
+// randomImage.src = ad  
--- Week4/toDo.js
+++ Week4/toDo.js
@@ -5,8 +5,32 @@ let completed = document.getElementById('completed');
 
 addTaskBtn.addEventListener('click', function() {
   let task = taskNameText.value;
-  
-  let liItem = document.createElement('li');
-  liItem.innerHTML = task;
-  taskList.appendChild(liItem);
+  let taskNameLabel = document.createElement('label');
+  taskNameLabel.innerHTML = task
+  // Add check box
+  let checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  // Add remove button
+  let removeBtn = document.createElement('button')
+  removeBtn.innerHTML = 'Remove';
+  removeBtn.addEventListener('click', () => {
+     // console.log(this)
+    taskList.removeChild(li)
+  })
+  // Create li
+  let li = document.createElement('li');
+
+  li.appendChild(checkBox);
+  li.appendChild(taskNameLabel)
+  li.appendChild(removeBtn);
+
+  taskList.appendChild(li);
 });
+
+checkBox.addEventListener('change', function() {
+  if (this.checked) {
+
+  } else {
+    
+  }
+})
